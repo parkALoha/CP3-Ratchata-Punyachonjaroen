7b859bee822395a5da2a112ceba0eae3341397ca
parkALoha <ratchata4205@gmail.com>
2021-03-18 23:06:04 +0700

    Update Exercise4_Ratchata_P.py

---
+++ Exercise4_Ratchata_P.py
@@ -0,0 +1,9 @@
+a= 27.5
+b=23.5
+c=24.0
+d=15.5
+
+print ("Foundation English : ",a)
+print ("General Business : ",b)
+print ("Introduction to Computer Systems : ",c)
+print ("Computer Programming : ",d)
