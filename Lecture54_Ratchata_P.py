097b7a58e17345eac25fd9f430db74feebfab188
parkALoha <ratchata4205@gmail.com>
2021-03-28 23:16:28 +0700

    Create Lecture54_Ratchata_P.py

---
+++ Lecture54_Ratchata_P.py
@@ -0,0 +1,38 @@
+def login():
+    usernameInput = input("Username : ")
+    passwordInput = input("Password : ")
+    if usernameInput == "admin" and passwordInput == "1234":
+        return True
+    else:
+        return False
+
+def showMenu():
+    print("----- iShop -----")
+    print("1. Vat Calculator")
+    print("2. Price Calculator")
+
+def menuSelect():
+    userSelected = int(input(">>"))
+    return userSelected
+
+def vatCalculator(totalPrice):
+    vat = 7
+    result = totalPrice + (totalPrice * vat / 100)
+    return result
+
+def priceCalculator():
+    price1 = int(input("First Product Price : "))
+    price2 = int(input("Second Product Price : "))
+    return vatCalculator(price1 + price2)
+
+if login():
+    showMenu()
+    result = menuSelect()
+    if result == 1:
+        price = int(input("Price (THB) : "))
+        print(vatCalculator(price))
+    elif result == 2:
+        print(priceCalculator())
+else:
+    print("Wrong username or password!")
+print("Thank you")
